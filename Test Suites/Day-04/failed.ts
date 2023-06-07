f412f1ff8caf908b5cd1937adcc6dbc003936665
deep-simform <deep.p@simformsolutions.com>
2023-06-07 14:28:55 +0530

    Integrating JIRA

---
+++ Test Suites/Day-04/failed.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>failed</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e882315e-294d-4f38-a7d3-62533ec7d917</testSuiteGuid>
+   <testCaseLink>
+      <guid>637beb2f-bb6c-4c5c-9854-6db2c5697a56</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day-04/failesTestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
